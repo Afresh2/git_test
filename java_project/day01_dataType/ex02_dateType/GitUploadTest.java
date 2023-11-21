ee118f6a6adea6bef1186efae30230e244f14870
Afresh2 <afresh320@daum.net>
2023-11-21 10:43:34 +0900

    update source

---
+++ java_project/day01_dataType/ex02_dateType/GitUploadTest.java
@@ -0,0 +1,11 @@
+package day01_dataType.ex02_dateType;
+
+public class GitUploadTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("git");
+
+	}
+
+}
